1cf1bbff7b454e7476a490424d7ac5205a03dd2b
Liceey001 <aliceedegwarhe@gmail.com>
2022-10-16 16:28:45 -0700

    upper cases

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,25 +10,27 @@
 int main(void)
 {
 	int n;
-	int x; |
+	int a;
 
-	srand(time(0));
+
+	srand(time(0);
 	n = rand() - RAND_MAX / 2;
-	x = n % 10;
-	printf("last digit of %d is %d", n, x);
-	if (x > 5)
-{
-	printf("and is greater than 5");
-}
-	if (x == 0)
-{
-	printf("and is 0");
-}
-	if (x < 6 && x != 0)
-{
-	printf("and is less than 6 and not 0");
-}
-	printf("\");
+	a = n % 10;
 
+	if (a > 5)
+	{
+	
+	      printf("Last digit of %d is %d and is greater than 5\n", n, a);
+	}
+	else if (a == 0)
+	{
+       	
+	       printf("Last digit of %d is %d and is 0\n", n, a);
+	}
+	else
+	{
+	     
+	   printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
+	}
 	return (0);
-}
+}}
